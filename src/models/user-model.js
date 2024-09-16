20fde8ea5881da3a0ad0571397023029a3884e4a
ShivamMishra828 <shivam9878522087@gmil.com>
2024-09-16 12:04:26 +0530

    FEAT: Added User Model with Password Hashing functionality

---
+++ src/models/user-model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            index: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        lastLogin: {
+            type: Date,
+            default: null,
+        },
+        notes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Note",
+            },
+        ],
+        resetPasswordToken: String,
+        resetPasswordExpiresAt: Date,
+        verificationCode: String,
+        verificationCodeExpiresAt: Date,
+    },
+    { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+});
+
+userSchema.methods.checkPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
